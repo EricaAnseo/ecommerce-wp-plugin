aa0ed52ab62442521cebeee1f8cde84b6bc1aa48
Erica Chai <Erica Chai>
2017-03-19 19:16:06 +0000

    View and Delete Products

--- includes/sub_menu_functions/sub_view_product.php
+++ includes/sub_menu_functions/sub_view_product.php
@@ -8,29 +8,57 @@ function simp_ec_view_products_page_html()
 
 	$results = $wpdb->get_results( 'SELECT * FROM ' . $table_product);
 	$no_of_products = $wpdb->get_var( 'SELECT COUNT(*) FROM '  . $table_product);
-
-	echo "<p>Products added {$no_of_products}</p>";
 	?>
 
-	<p>Number of products added <?php echo $no_of_products ?></p>
-
+	<h1>Products</h1>
+	<table>
+		<tr>
+			<th></th>
+			<th>Name</th>
+			<th>SKU</th>
+			<th>Description</th>
+			<th>Short Description</th>
+			<th>Price</th>
+			<th></th>
+		</tr>
 <?php
+
 	if($results)
 	{
+
 		foreach ( $results as $product )
-		{ 
-			?>
-			<p>Product Name: <?php echo $product->pname ?> </p>
+		{ ?>
+			
+			<tr>
+				<td><input type="checkbox" name="bulk-delete[]" value="<?php echo $product->product_id ?>" /></td>
+				<td><?php echo $product->pname ?></td>
+				<td><?php echo $product->product_sku ?></td>
+				<td><?php echo $product->pdesc ?></td>
+				<td><?php echo $product->pshortdesc ?></td>
+				<td><?php echo $product->pprice ?></td>
+				<td>
+					<form id="delete_product_form" action="#delete-product" method="post" onsubmit="return confirm('Are you sure you want to delete this product?');"">
+						<input type="submit" name="Delete" value="Delete" class="button button-primary" />
+						<input type='hidden' name="product_id" value="<?php echo $product->product_id ?>"/>
+					</form>
+				</td>
+			</tr>
 
-		<?php } 
-	}
-	else
-	{
+		<?php }
 
 	}
+
+
 ?>
-<p></p>
+	</table>
+	<p>Number of products added <?php echo $no_of_products ?></p>
 <?php
+	if(isset($_POST['product_id']))	{ 
+		$id = $_POST['product_id'];
+		$table_product = $wpdb->prefix . "simp_ec_product"; 
+		$wpdb->delete( $table_product, array( 'product_id' => $id ) );
+		echo "<meta http-equiv='refresh' content='0'>";
+	}
 
 }
 
